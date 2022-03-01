773a4386ab414d836b43c9c47dfbceb99dbb53e7
Gabriel Novais <silvanovaesgabriel@gmail.com>
2022-03-01 14:47:17 -0300

    Service layer, component registration

--- src/main/java/com/example/projectjpa/resources/UserResource.java
+++ src/main/java/com/example/projectjpa/resources/UserResource.java
@@ -1,20 +1,33 @@
 package com.example.projectjpa.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.projectjpa.entities.User;
+import com.example.projectjpa.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource
 {
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll()
+	public ResponseEntity<List<User>> findAll()
 	{
-		User u = new User(1L, "Maria", "maria@gmail.com", "999999", "12345");
-		return ResponseEntity.ok().body(u);
+		return ResponseEntity.ok().body(service.findAll());
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id)
+	{
+		return ResponseEntity.ok().body(service.findById(id));
 	}
 }
---
+++ src/main/java/com/example/projectjpa/services/UserService.java
@@ -0,0 +1,26 @@
+package com.example.projectjpa.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.projectjpa.entities.User;
+import com.example.projectjpa.repositories.UserRepository;
+
+@Service
+public class UserService
+{
+	@Autowired
+	private UserRepository userRepository;
+	
+	public List<User> findAll()
+	{
+		return userRepository.findAll();
+	}
+	
+	public User findById(Long id)
+	{
+		return userRepository.findById(id).get();
+	}
+}
